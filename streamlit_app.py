9964a95628e4ea5fa6170d5a31ed6589216d31ef
Vinothkumar M <50379169+vinothtrue@users.noreply.github.com>
2022-10-13 20:46:30 +0530

    Update streamlit_app.py
    Added new items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 streamlit.title('My Parens New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluebery Oatmeal')
-streamlit.text('Kale, Spinash & Rocket Smoothie')
-streamlit.text('Hard-Boiled free-Range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Bluebery Oatmeal')
+streamlit.text('🥗 Kale, Spinash & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled free-Range Egg')
+streamlit.text('🥑🍞 Avocoda Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
